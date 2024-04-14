73d7e76bdb20ac3fbe56f5ba63ccef8ee32c42b4
Ujjawal Prasad <ujjawalprasad111@gmail.com>
2024-04-14 13:40:00 -0700

    Add files via upload
    better model, only contains model code instead of everything

--- model.py
+++ model.py
@@ -9,8 +9,7 @@ import sklearn.ensemble as ske
 import sklearn.neural_network as skn
 from sklearn.model_selection import train_test_split
 
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+#dataframe is in data_preprocessing
 
 from subprocess import check_output
 print(check_output(["ls", "../Wildfire_Cause_Prediction/"]).decode("utf8"))
@@ -20,53 +19,17 @@ print(check_output(["ls", "../Wildfire_Cause_Prediction/"]).decode("utf8"))
 cnx = sqlite3.connect('../Wildfire_Cause_Prediction/FPA_FOD_20170508.sqlite')
 
 df = pd.read_sql_query("SELECT FIRE_YEAR,STAT_CAUSE_DESCR,LATITUDE,LONGITUDE,STATE,DISCOVERY_DATE,FIRE_SIZE FROM 'Fires'", cnx)
-#print(df.head()) #check the data
 
 df['DATE'] = pd.to_datetime(df['DISCOVERY_DATE'] - pd.Timestamp(0).to_julian_date(), unit='D')
-#print(df.head()) #check the data
 
 df['MONTH'] = pd.DatetimeIndex(df['DATE']).month
 df['DAY_OF_WEEK'] = df['DATE'].dt.day_name()
 df_orig = df.copy() #I will use this copy later
-#print(df.head())
 
-"""
-df_arson = df[df['STAT_CAUSE_DESCR']=='Arson']
-df_arson['DAY_OF_WEEK'].value_counts().plot(kind='barh',color='coral')
-plt.show()
-
-df['STATE'].value_counts().head(n=10).plot(kind='barh',color='coral')
-plt.show()
-
-df_lightning = df[df['STAT_CAUSE_DESCR']=='Lightning']
-df_lightning['DAY_OF_WEEK'].value_counts().plot(kind='barh',color='coral')
-plt.show()
-
-df['DAY_OF_WEEK'].value_counts().plot(kind='barh',color='coral')
-plt.show()
-
-df['STAT_CAUSE_DESCR'].value_counts().plot(kind='barh',color='coral')
-plt.show()
-
-df.plot(kind='scatter',x='LONGITUDE',y='LATITUDE',color='coral',alpha=0.3)
-plt.show()
-"""
 le = preprocessing.LabelEncoder()
 df['STAT_CAUSE_DESCR'] = le.fit_transform(df['STAT_CAUSE_DESCR'])
 df['STATE'] = le.fit_transform(df['STATE'])
 df['DAY_OF_WEEK'] = le.fit_transform(df['DAY_OF_WEEK'])
-#print(df.head())
-
-def plot_corr(df,size=10):
-    corr = df.corr()  #the default method is pearson
-    fig, ax = plt.subplots(figsize=(size, size))
-    ax.matshow(corr,cmap=plt.cm.Oranges)
-    plt.xticks(range(len(corr.columns)), corr.columns)
-    plt.yticks(range(len(corr.columns)), corr.columns)
-    for tick in ax.get_xticklabels():
-        tick.set_rotation(45)
-    plt.show()
-
 
 df = df.drop('DATE',axis=1)
 df = df.dropna()
@@ -75,7 +38,6 @@ X = df.drop(['STAT_CAUSE_DESCR'], axis=1).values
 y = df['STAT_CAUSE_DESCR'].values
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3, random_state=0) #30% for testing, 70% for training
-
 
 def set_label(cat):
     cause = 0
@@ -96,9 +58,7 @@ def set_label(cat):
 
 df['LABEL'] = df_orig['STAT_CAUSE_DESCR'].apply(lambda x: set_label(x)) # I created a copy of the original df earlier in the kernel
 df = df.drop('STAT_CAUSE_DESCR',axis=1)
-#print(df.head())
 
-"""
 n_estimators = [10, 100, 400, 700, 1000]
 criterion = ["gini", "entropy", "log_loss"]
 max_features = ["sqrt", "log2"]
@@ -114,42 +74,3 @@ clf_rf = clf_rf.fit(X_train, y_train)
 print(clf_rf.score(X_test,y_test))
 
 dump(clf_rf, 'model.joblib') 
-"""
-clf_rf = load('model.joblib')
-
-#pd.DataFrame(X_test).to_csv('X_test.csv')
-
-pred = clf_rf.predict_proba(X_test)
-X_test_df = pd.DataFrame(X_test)
-predictions = pd.DataFrame(pred, columns = ["natural", "accidental", "malicious", "other"])
-print(X_test_df)
-long_list = X_test_df[1].tolist()
-lat_list = X_test_df[2].tolist()
-predictions2 = predictions.assign(longitude = long_list)
-predictions3 = predictions.assign(latitude = lat_list)
-
-print(predictions3)
-predictions3.to_csv('X_test_predictions.csv')
-"""for x in n_estimators:
-    for y in criterion:
-        for z in max_features:
-            clf_rf = ske.RandomForestClassifier(n_estimators=x, criterion = y, max_features = z)
-            clf_rf = clf_rf.fit(X_train, y_train)
-            print(clf_rf.score(X_test,y_test))
-            print("n_estimators = " + str(x) + " | criterion = " + str(y) + " | max_features = " + str(z) + " |")
-
-from sklearn.metrics import confusion_matrix
-y_pred = clf_rf.fit(X_train, y_train).predict(X_test)
-cm = confusion_matrix(y_true=y_test,y_pred=y_pred)
-print(cm)
-
-cmn = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-fig,ax = plt.subplots(figsize=(10,10))
-ax.matshow(cmn,cmap=plt.cm.Oranges,alpha=0.7)
-for i in range(cmn.shape[0]):
-    for j in range(cmn.shape[1]):
-        ax.text(x=j,y=i,s=cmn[i,j],va='center',ha='center')
-plt.xlabel('predicted label')
-plt.ylabel('true label')
-plt.show()
-"""
